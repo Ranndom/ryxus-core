fef0d8ccd875cdc5cae0e7f0f6edd0555f936b4a
Ranndom <ranndom@rnndm.xyz>
2016-02-21 14:46:27 +1000

    Add API server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,48 @@
 'use strict';
+
+import restify from 'restify';
+import bunyan from 'bunyan';
+import requireDir from 'require-dir';
+import config from '../config/config.json';
+
+const routes = requireDir('controllers');
+
+var log = bunyan.createLogger({
+    name: 'ryxus_api',
+    level: process.env.LOG_LEVEL || 'info',
+    stream: process.stdout,
+    serializers: bunyan.stdSerializers
+});
+
+/*
+ * Restify app
+*/
+var server = restify.createServer({
+    name: 'ryxus_api',
+    log: log
+});
+
+/*
+ * Middleware
+ */
+server.use(restify.bodyParser({ mapParams: false }));
+server.use(restify.queryParser());
+server.use(restify.gzipResponse());
+server.use(restify.requestLogger());
+
+server.pre(restify.pre.sanitizePath());
+
+server.on('after', restify.auditLogger({log: log}));
+
+/*
+ * Routes
+ */
+
+routes.main.applyRoutes(server);
+
+/*
+ * Server startup
+ */
+server.listen(config.api.port || 3000, function() {
+    log.info("%s listening at %s", server.name, server.url);
+});
